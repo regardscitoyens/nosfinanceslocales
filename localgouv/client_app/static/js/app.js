26ba606d89f6dfb4fc4e2c69b56ab266c3a9b73f
François Massot <francois.massot@gmail.com>
2014-01-10 03:05:33 +0100

    WIP client

--- localgouv/client_app/static/js/app.js
+++ localgouv/client_app/static/js/app.js
@@ -1,14 +1,55 @@
-angular.module('app', ['ui.router'])
+angular.module('app', ['ui.router', 'ui.bootstrap'])
+    .constant('CLOUDMADE_API_KEY', '96d88d05d11e4ce4b841290fa31277fb')
     .constant('API_ROOT_URL', '/api')
+    .constant('TILES_ROOT_URL', 'http://www.localfinance.fr/tiles') // get this info from server ?
+    .constant('THUMBNAILS_URL', 'http://www.localfinance.fr/static/thumbnails')
+    .factory('mapUtils', function(TILES_ROOT_URL, THUMBNAILS_URL) {
+        return {
+            getTileUrl: function(map_id) {
+                return TILES_ROOT_URL + '/' + map_id + "/{z}/{x}/{y}.png";
+            },
+            getGridUrl: function(map_id) {
+                return TILES_ROOT_URL + '/' + map_id + "/{z}/{x}/{y}.grid.json";
+            },
+            getThumbnailUrl: function(map_id) {
+                return THUMBNAILS_URL + '/' + map_id + '.png';
+            }
+        };
+    })
+    .factory('Resource', function($http, API_ROOT_URL) {
+        function Factory(name) {
+            var resource = {
+                get: function(id) {
+                    return $http.get(API_ROOT_URL + '/' + name + '/' + id)
+                        .then(function(response) {
+                            return response.data.results;
+                        })
+                },
+                all: function() {
+                    return $http.get(API_ROOT_URL + '/' + name + 's')
+                        .then(function(response) {
+                            return response.data.results;
+                        });
+                }
+            }
+            return resource;
+        }
+        return Factory
+    })
     .config(
         [ '$stateProvider', '$urlRouterProvider',
         function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider
-                .otherwise('/maps');
+            $urlRouterProvider.when('', 'maps');
+            $urlRouterProvider.otherwise('maps');
             $stateProvider
                 .state('home', {
                     url: '/',
                     templateUrl: 'templates/home.html',
+                    resolve: {
+                        stats: function(Resource) {
+                            return Resource('stat').all();
+                        }
+                    }
                 })
                 .state('about', {
                     url: '/about',
@@ -26,9 +67,14 @@ angular.module('app', ['ui.router'])
                 .state('maps', {
                     abstract: true,
                     url: '/maps',
-                    views: {
-                        '': {
-                            templateUrl: 'templates/maps.html',
+                    templateUrl: 'templates/maps.html',
+                    controller: 'MapsCtrl',
+                    resolve: {
+                        timemaps: function(Resource) {
+                            return Resource('timemap').all()
+                        },
+                        stats: function(Resource) {
+                            return Resource('stat').all();
                         }
                     }
                 })
@@ -42,7 +88,7 @@ angular.module('app', ['ui.router'])
                     }
                 })
                 .state('maps.detail', {
-                    url: '/{id}',
+                    url: '/{var_name}',
                     views: {
                         '': {
                             templateUrl: 'templates/map.detail.html',
@@ -52,75 +98,199 @@ angular.module('app', ['ui.router'])
                 });
         }])
     .controller('Home', ['$scope', '$state',
-        function($scope, $state) {
+        function($scope, $state, stats) {
             $scope.$state = $state;
+            $scope.stats = stats;
     }])
-    .controller('MapsCtrl', ['$scope', '$state', '$http',
-        function($scope, $state, $http, maps) {
+    .controller('MapsCtrl', ['$scope', '$state', 'mapUtils', 'timemaps', 'stats',
+        function($scope, $state, mapUtils, timemaps, stats) {
+            $scope.mapUtils = mapUtils;
+            $scope.timemaps = timemaps;
+            $scope.stats = stats;
         }])
-    .controller('MapListCtrl', ['$scope', '$state', '$http', 'API_ROOT_URL',
-        function($scope, $state, $http, API_ROOT_URL) {
-            $http.get(API_ROOT_URL + '/timemaps')
-                .success(function(data){
-                    $scope.maps = data.results;
-            });
+    .controller('MapListCtrl', ['$scope', '$state', '$timeout',
+        function($scope, $state, $timeout) {
+            $scope.thumbailUrls = [];
+            function fireDigestEverySecond() {
+                $timeout(fireDigestEverySecond , 1000);
+            };
+            fireDigestEverySecond();
         }])
-    .controller('MapDetailCtrl', ['$scope', '$state', '$http',
-        function($scope, $state, $http) {
-            $scope.mapData = {
-                baseTileUrl: "http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/998/256/{z}/{x}/{y}.png",
-                tileUrl: "http://www.localfinance.fr/tiles/debt_per_person_2007/{z}/{x}/{y}.png",
-                gridUrl: 'http://{s}.tiles.mapbox.com/v3/mapbox.geography-class/{z}/{x}/{y}.grid.json?callback={cb}'
+    .controller('MapDetailCtrl', ['$scope', '$stateParams', '$http', 'mapUtils',
+        function($scope, $stateParams, $http, mapUtils) {
+            // get the timemap for this variable
+            $scope.timemap = $scope.timemaps.filter(function(timemap) {
+                return (timemap.var_name == $stateParams.var_name)
+            })[0];
+            // same for stats
+            $scope.stat = $scope.stats.filter(function(stat) {
+                return (stat.var_name == $stateParams.var_name)
+            })[0];
+            // take first map
+            $scope.year = $scope.timemap.maps[0].year;
+            $scope.opacity = 0.85;
+            function findMapData(year){
+                return $scope.timemap.maps.filter(function(m) {
+                    return (m.year == year)
+                })[0];
             }
+            $scope.mapData = findMapData($scope.timemap.maps[0].year);
+            $scope.$watch('year', function(newVal, oldVal) {
+                $scope.mapData = findMapData(newVal);
+            });
             $scope.onClick = function(data) {
-                console.log(e.data);
+                $scope.$apply();
             }
             $scope.onMouseOver = function(data) {
-                console.log("yo");
+                $scope.mouseOverdata = data;
+                $scope.$apply();
             }
         }])
-    .directive('leafletMap', function () {
+    .directive('leafletMap', function (mapUtils, CLOUDMADE_API_KEY) {
         return {
             restrict: "A",
             replace: false,
             scope: {
                 mapData: '=',
+                opacity: '=',
                 click: '&onClick',
                 mouseOver: '&onMouseOver',
-                mouseOut: '&onMouseOut',
+                mouseOut: '&onMouseOut'
             },
             link: function($scope, element, attrs, controller) {
                 var onClick = $scope.click(),
                     onMouseOver = $scope.mouseOver(),
                     onMouseOut = $scope.mouseOut();
-                element[0].width = '100%';
-                var cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade',
-                    cloudmade = new L.TileLayer($scope.mapData.baseTileUrl, {attribution: cloudmadeAttribution}),
-                    mylyr = new L.TileLayer($scope.mapData.tileUrl),
-                    utfGrid = new L.UtfGrid($scope.mapData.gridUrl);
 
-                var interactiveLayerGroup = L.layerGroup([cloudmade, mylyr, utfGrid]);
+                $scope.$watch('mapData', function(newVal, oldVal) {
+                    return $scope.render(newVal);
+                }, true);
+
+                var southWest = L.latLng($scope.mapData.extent[1],
+                                         $scope.mapData.extent[0]),
+                    northEast = L.latLng($scope.mapData.extent[3],
+                                         $scope.mapData.extent[2]),
+                    bounds = L.latLngBounds(southWest, northEast);
+
+                var basemap = new L.TileLayer("http://{s}.tile.stamen.com/toner" +
+                        "/{z}/{x}/{y}.png")
+                    mylyr = new L.TileLayer(mapUtils.getTileUrl($scope.mapData.id),
+                        {opacity: $scope.opacity, maxBounds: bounds}),
+                    utfGrid = new L.UtfGrid(mapUtils.getGridUrl($scope.mapData.id),
+                                            {useJsonP: false});
+
+                var interactiveLayerGroup = L.layerGroup(
+                    [basemap, mylyr, utfGrid]
+                );
 
                 //Events
                 utfGrid.on('click', function (e) {
                     if (e.data) {
-                        onClick(e.data)
-                    } else {
+                        onClick(e.data);
                     }
                 });
                 utfGrid.on('mouseover', function (e) {
                     if (e.data) {
-                        onMouseOver(e.data)
-                    } else {
+                        onMouseOver(e.data);
                     }
                 });
                 utfGrid.on('mouseout', function (e) {
-                    onMouseOut()
                 });
 
-                //Create our map with just the base TileLayer
-                var map = L.map(element[0])
-                                .setView([0, 0], 1)
-                                .addLayer(interactiveLayerGroup);
+                var options = {
+                    center: L.latLng(46.22587886848165, 2.21040301636657),
+                    zoom: 6,
+                    minZoom: $scope.mapData.minzoom,
+                    maxZoom: $scope.mapData.maxzoom
+                };
+                map = L.map(element[0], options)
+                    .addLayer(interactiveLayerGroup);
+
+                $scope.render = function(mapData) {
+                    mylyr.setUrl(mapUtils.getTileUrl(mapData.id));
+                    map.removeLayer(utfGrid);
+                    utfGrid = new L.UtfGrid(mapUtils.getGridUrl($scope.mapData.id),
+                                            {useJsonP: false});
+                    map.addLayer(utfGrid);
+                    //Events
+                    utfGrid.on('click', function (e) {
+                        if (e.data) {
+                            onClick(e.data);
+                        }
+                    });
+                    utfGrid.on('mouseover', function (e) {
+                        if (e.data) {
+                            onMouseOver(e.data);
+                        }
+                    });
+                    utfGrid.on('mouseout', function (e) {
+                    });
+
+                    }
             }
-        }});
+        }})
+    .directive('lineChart', function () {
+        return {
+            restrict: "A",
+            replace: false,
+            scope: {
+                data: '=',
+            },
+            link: function($scope, element, attrs, controller) {
+                var tooltip = d3.select(element[0])
+                    .append("div")
+                    .attr("class", "tooltip")
+                    .style("opacity", 1);
+                var margin = {top: 20, right: 20, bottom: 30, left: 50},
+                    width = 400 - margin.left - margin.right,
+                    height = 250 - margin.top - margin.bottom;
+                var parseDate = d3.time.format("%Y").parse;
+                $scope.render = function(data) {
+                    var x = d3.time.scale()
+                        .range([0, width])
+                        .domain(d3.extent(data, function(d){return parseDate(d[0].toString())}));
+                    var y = d3.scale.linear()
+                        .range([height, 0])
+                        .domain(d3.extent(data, function(d){return d[1]}));
+                    //Create SVG element
+                    var line = d3.svg.line()
+                        .x(function(d){return x(parseDate(d[0].toString()))})
+                        .y(function(d){return y(d[1])});
+                    var svg = d3.select(element[0])
+                        .append("svg")
+                        .attr("width", width + margin.left + margin.right)
+                        .attr("height", height + margin.top + margin.bottom)
+                        .append("g")
+                        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                    var xAxis = d3.svg.axis()
+                        .scale(x)
+                        .orient('bottom')
+                        .ticks(d3.time.years, 5);
+                    svg.append("g")
+                        .attr('class', 'x axis')
+                        .attr("transform", "translate(0," + height + ")")
+                        .call(xAxis);
+                    var yAxis = d3.svg.axis().scale(y).orient("left");
+                    svg.append("g")
+                        .attr('class', 'y axis')
+                        .call(yAxis);
+                    svg.append("path")
+                        .datum(data)
+                        .attr("class", "line")
+                        .attr("d", line);
+                    svg.selectAll('circle')
+                        .data(data)
+                        .enter()
+                        .append('circle')
+                        .attr("r",5)
+                        .attr("cx", function(d) { return x(parseDate(d[0].toString())); })
+                        .attr("cy", function(d) { return y(d[1]); })
+                        .attr("class", "linecircle")
+                        .style("pointer-events","all")
+                        .append('title')
+                        .text(function(d) { return d[0] + ' : ' + d[1];});
+                }
+                $scope.render($scope.data);
+            }
+        }
+    });
