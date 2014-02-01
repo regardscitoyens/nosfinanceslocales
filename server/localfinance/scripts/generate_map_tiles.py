3568b752e378805666e69890464e8be693eda10a
François Massot <francois.massot@gmail.com>
2014-02-01 19:16:40 +0100

    Improve map tiles script

--- server/localfinance/scripts/generate_map_tiles.py
+++ server/localfinance/scripts/generate_map_tiles.py
@@ -19,7 +19,7 @@ from ..models import (
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
-    print('usage: %s <config_uri> [var=value]\n'
+    print('usage: %s <config_uri> map_ids\n'
           '(example: "%s development.ini")' % (cmd, cmd))
     sys.exit(1)
 
@@ -27,6 +27,7 @@ def main(argv=sys.argv):
     if len(argv) < 2:
         usage(argv)
     config_uri = argv[1]
+    map_ids = argv[2].split(',')
     setup_logging(config_uri)
     settings = get_appsettings(config_uri)
     engine = engine_from_config(settings, 'sqlalchemy.')
@@ -34,9 +35,10 @@ def main(argv=sys.argv):
 
     years = zip(*DBSession.query(AdminZoneFinance.year).distinct().order_by("year").all())[0]
 
-    for var_name in MAPS_CONFIG.keys():
+    map_ids = MAPS_CONFIG.keys() if map_ids[0] == 'ALL' else map_ids
+    for map_id in map_ids:
         for year in years:
-            m = Map(year, var_name)
+            m = Map(year, map_id)
             extent = m.info['extent']
             xmlmap = carto_convert(m.mapnik_config)
             map_tile_dir = os.path.join(settings['base_tile_dir'], m.info['id']) + '/'
@@ -44,7 +46,7 @@ def main(argv=sys.argv):
                 os.makedirs(map_tile_dir)
             except OSError:
                 pass
-            render_tiles(extent, xmlmap, map_tile_dir, m.info['minzoom'], m.info['maxzoom'], name=m.info['name'], fields=[var_name, 'name', 'code_insee', 'id', 'year'], layer_id=0)
+            render_tiles(extent, xmlmap, map_tile_dir, m.info['minzoom'], m.info['maxzoom'], name=m.info['name'], fields=[map_id, 'name', 'code_insee', 'id', 'year'], layer_id=0)
 
 
 if __name__ == '__main__':
